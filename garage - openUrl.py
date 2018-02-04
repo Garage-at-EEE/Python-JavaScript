dd9848b7647b597922434ffb333354ce6eea3014
Zayn Jarvis <zaynjarvis@outlook.com>
2018-02-04 23:56:10 +0800

    openUrl py

--- garage - openUrl.py
+++ garage - openUrl.py
@@ -1,5 +1,22 @@
 import webbrowser
-
+listDic = {}
 with open("Reference.txt",'r') as file:
-	for url in file.read().split('\n'):
-		webbrowser.open(url)
+	url_list = file.read().split('\n')
+	for item in url_list:
+		listDic[item.split('\t')[0]] = item.split('\t')[1]
+def openUrl(target = 0):
+	for key, value in listDic.items():
+		if target == 0 or target >= len(listDic.keys()):
+			webbrowser.open(item)
+		elif str(target) == key[0]:
+			webbrowser.open(value)
+
+
+def viewUrl():
+	for key in listDic.keys():
+		print(f'Title: {key}\nUrl : {listDic[key]}')
+		print()
+
+viewUrl()
+print("which file to open? use index number")
+openUrl(int(input()))
